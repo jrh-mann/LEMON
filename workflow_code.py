2cb935157ed7aca6f6433e904ae81a869010d81c
Jude <jude.hawrani@gmail.com>
2026-01-07 11:04:28 -0500

    removed punctuation

--- workflow_code.py
+++ workflow_code.py
@@ -11,39 +11,39 @@ def determine_workflow_outcome(inputs):
 
     if total_cholesterol > 7.5:
         if under_specialist_clinic:
-            return "No Further Action"
+            return "no further action"
         else:
             if optimised_on_treatment:
-                return "Consider Lipid Clinic Referral"
+                return "consider lipid clinic referral"
             else:
-                return "Send \"Lipid Lowering Therapy\" AccuRx with self booking link"
+                return "send lipid lowering therapy accurx with self booking link"
     elif total_cholesterol > 5 and total_cholesterol <= 7.5:
         # raised
         if taking_lipid_lowering_therapy:
             if optimised_on_treatment:
-                return "Mark as \"No Further Action\""
+                return "mark as no further action"
             else:
-                return "Send \"Lipid Lowering Therapy\" AccuRx with self booking link"
+                return "send lipid lowering therapy accurx with self booking link"
         else:
             if prevention_type == "Primary":
                 if qrisk2_3_score >= 10:
-                    return "Send \"Lipid Lowering Therapy\" AccuRx with self booking link"
+                    return "send lipid lowering therapy accurx with self booking link"
                 else:
                     # <10%
-                    return "Send \"High Cholesterol - Low QRISK\" AccuRx with self booking link"
+                    return "send high cholesterol low qrisk accurx with self booking link"
             else:
                 # secondary
                 if taking_maximally_tolerated_statin:
                     if ldl_cholesterol <= 2:
-                        return "No Further Action"
+                        return "no further action"
                     elif ldl_cholesterol > 2 and ldl_cholesterol <= 2.5:
-                        return "Add in Ezetimibe"
+                        return "add in ezetimibe"
                     else:
                         # >2.6
-                        return "Initiate Inclisiran"
+                        return "initiate inclisiran"
                         # then you neede to wait 3 months and check ldl again, not sure how to do this here
                 else:
-                    return "Send \"Lipid Lowering Therapy\" AccuRx with self booking link"
+                    return "send lipid lowering therapy accurx with self booking link"
     else:
         # normal
-        return "Mark as Satisfactory"
+        return "mark as satisfactory"
