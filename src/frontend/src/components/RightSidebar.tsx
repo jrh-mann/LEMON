cbe82e51a7f5e22010d05bab96f3c924d2a36088
axelcbw <axelweernink228@gmail.com>
2026-03-09 22:00:18 +0000

    fix right sidebar variables panel JSX

--- src/frontend/src/components/RightSidebar.tsx
+++ src/frontend/src/components/RightSidebar.tsx
@@ -414,7 +414,7 @@ export default function RightSidebar() {
                   + Add Variable
                 </button>
               </div>
-            ) : null}
+            )}
           </>
         )}
       </aside>
